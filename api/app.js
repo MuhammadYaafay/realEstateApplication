1a3f0616cfcd833323e518d259eaed5360051c36
Yaafay <muhammad.yaffay@gmail.com>
2025-03-16 19:32:41 +0500

    initialize API structure with authentication and post routes

---
+++ api/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+import dotenv from "dotenv";
+
+dotenv.config(); // Loads environment variables from .env file
+
+const app = express();
+
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
---
+++ api/routes/post.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.get("/test", (req, res) => {
+//   res.send("It's working!");
+// });
+
+// router.post("/test", (req, res) => {
+//   res.send("It's working!");
+// });
+
+// router.put("/test", (req, res) => {
+//   res.send("It's working!");
+// });
+
+// router.delete("/test", (req, res) => {
+//   res.send("It's working!");
+// });
+
+export default router;
