ad5df91a5448e78b379534ddc19f4e66e95c8ef5
Yaafay <muhammad.yaffay@gmail.com>
2025-03-17 22:28:45 +0500

    add axios and cors dependencies; implement authentication routes and middleware

--- api/app.js
+++ api/app.js
@@ -1,18 +1,22 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 import dotenv from "dotenv";
 
 dotenv.config(); // Loads environment variables from .env file
 
 const app = express();
 
-app.use(cookieParser())
+app.use(cors({origin:process.env.CLIENT_URL, credentials: true}));
+app.use(cookieParser()); 
 app.use(express.json());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
